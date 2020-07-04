d85bddb4c3ef4c9f35d32b6e8347047c25702d15
bellrise <65973858+bellrise@users.noreply.github.com>
2020-07-04 15:39:37 +0200

    Delete python_dungeon.py

--- python_dungeon.py
+++
@@ -1,7 +0,0 @@
-import os, random, sys
-import blacksmith, start, player_data, player_stats
-import event, username, main_screen, clear
-from time import sleep
-
-username.trigger()
-main_screen.trigger()
