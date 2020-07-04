35d2b0b5d6f0a1c8f6db1c937401050a247e51dd
bellrise <65973858+bellrise@users.noreply.github.com>
2020-07-04 23:51:16 +0200

    Added comments under each method and class

--- controller.py
+++ controller.py
@@ -2,22 +2,35 @@
 Takes the data from main.py and controls the user-system interactions.
 """
 import player
+import event
 
 
 class Controller:
     def __init__(self, username, difficulty):
-        # Global definitions
+        """
+        This is called when main.py finishes the setup.
+        The Player and EventHandler instance is created here
+        for use in main().
+        """
         self.difficulty = difficulty
 
         # Creates a new player
         self.Player = player.Player(username)
+        self.Event = event.EventHandler(username)
 
         # Starts the game
-        self.start()
+        self.main()
 
-    def start(self):
-        # Adventure time
-        print('Adventure time!')
+    def main(self):
+        """
+        This is the main method called when the program starts
+        place all game functionality here, external classes and
+        methods can be used.
+        """
+        print('\n\nAdventure time!')
 
-        self.Player.INVENTORY.add('paper', 12)
-        self.Player.INVENTORY.add('apple', 3)
+        # Game
+
+        self.Player.INVENTORY.add('coin', 250)
+
+        print(self.Player.INVENTORY)
