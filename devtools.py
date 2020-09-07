335dab5196b32288a0dc0f926e06dcf997cdea5b
Lotus <59749700+xyLotus@users.noreply.github.com>
2020-09-07 19:56:18 +0200

    Update devtools.py

--- devtools.py
+++ devtools.py
@@ -1,3 +1,10 @@
+from time import sleep
+import keyboard
+
 def hold_exit():
-    input('\n\n[PRESS ENTER TO EXIT]')
-    quit()
+    print("[PRESS ENTER TO EXIT]")
+    while 1:
+        sleep(0.05)
+        if keyboard.is_pressed("enter"):
+            quit()
+hold_exit()
